100e679eb3d490095e5e8f712059dcf1601e6fcb
rybasher <webleontev1@gmail.com>
2022-02-27 11:46:41 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const config  = require('config');
+console.log(config);
+
+
+app.get('/', (req, res) => {
+    res.send('cicd Sample v1')
+})
+
+app.get('/status', (req, res) => {
+    res.send('ok')
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
